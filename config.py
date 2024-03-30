07bf36f93212d087d85f140b53be965aa4cd60a1
AjDonna <89831798+AjDonna@users.noreply.github.com>
2024-03-30 12:58:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ API_ID = int(getenv("7603458"))
 API_HASH = getenv("910e420f1f74f40305a684a331dade35")
 BOT_TOKEN = getenv("7117010157:AAH_sS3WVBQA6wYrclWFLZGP7c6v7KZ3DMk")
 
-MONGO_DB_URI = getenv(mongodb+srv://BashBot02:BashBot@bashbot.8uzlxey.mongodb.net/?retryWrites=true&w=majority"")
+MONGO_DB_URI = getenv("mongodb+srv://BashBot02:BashBot@bashbot.8uzlxey.mongodb.net/?retryWrites=true&w=majority")
 
 INDEX_ID = int(getenv("-1001433720912"))
 UPLOADS_ID = int(getenv("-1001438374709"))
