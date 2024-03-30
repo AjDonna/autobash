bb9aa3024a35c63b6fb44f30f503378642a07327
AjDonna <89831798+AjDonna@users.noreply.github.com>
2024-03-30 12:57:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,18 +3,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_TOKEN = getenv("BOT_TOKEN")
+API_ID = int(getenv("7603458"))
+API_HASH = getenv("910e420f1f74f40305a684a331dade35")
+BOT_TOKEN = getenv("7117010157:AAH_sS3WVBQA6wYrclWFLZGP7c6v7KZ3DMk")
 
-MONGO_DB_URI = getenv("MONGO_DB_URI")
+MONGO_DB_URI = getenv(mongodb+srv://BashBot02:BashBot@bashbot.8uzlxey.mongodb.net/?retryWrites=true&w=majority"")
 
-INDEX_ID = int(getenv("INDEX_ID"))
-UPLOADS_ID = int(getenv("UPLOADS_ID"))
+INDEX_ID = int(getenv("-1001433720912"))
+UPLOADS_ID = int(getenv("-1001438374709"))
 
-STATUS_ID = int(getenv("STATUS_ID"))
-SCHEDULE_ID = int(getenv("SCHEDULE_ID"))
+STATUS_ID = int(getenv("-1001438374709"))
+SCHEDULE_ID = int(getenv("-1001438374709"))
 
-CHANNEL_TITLE = getenv("CHANNEL_TITLE")
-INDEX_USERNAME = getenv("INDEX_USERNAME")
-UPLOADS_USERNAME = getenv("UPLOADS_USERNAME")
+CHANNEL_TITLE = getenv("Anime_Bash")
+INDEX_USERNAME = getenv("@All_new_Doraemon_movies")
+UPLOADS_USERNAME = getenv("@Ongoing_Bash")
